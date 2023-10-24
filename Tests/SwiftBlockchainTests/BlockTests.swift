a5b93a3470bca6d03f3d2ed9a1a8c85a70f90a22
R. Kukuh <rkukuh@gmail.com>
2023-10-24 23:14:53 +0700

    Change the Block's test case to fit new Genesis block's data

--- Tests/SwiftBlockchainTests/BlockTests.swift
+++ Tests/SwiftBlockchainTests/BlockTests.swift
@@ -8,35 +8,19 @@
 import XCTest
 @testable import SwiftBlockchain
 
-class BlockTests: XCTestCase {
+final class BlockTests: XCTestCase {
     
     var block: Block!
     
-    override func setUp() {
-        super.setUp()
-        
-        block = Block(index: 1, 
-                      previousHash: "000000",
-                      timestamp: 1633027200,
-                      data: "Test Data",
-                      hash: "123456",
-                      nonce: 0)
-    }
-    
-    override func tearDown() {
-        block = nil
-        
-        super.tearDown()
-    }
-    
     func testGenesisBlock() {
         let genesis = Block.genesis
+        
         XCTAssertEqual(genesis.index, 0)
         XCTAssertEqual(genesis.previousHash, "0")
-        XCTAssertEqual(genesis.timestamp, 1508270000000)
+        XCTAssertEqual(genesis.timestamp, 1697293989000)
         XCTAssertEqual(genesis.data, "Genesis Block created")
-        XCTAssertEqual(genesis.hash, "000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf")
-        XCTAssertEqual(genesis.nonce, 604)
+        XCTAssertEqual(genesis.hash, "0000cc157676415ae96d7c678abff10dc34a27171f04fc0c49e47115e863e85a")
+        XCTAssertEqual(genesis.nonce, 123)
     }
     
 }
